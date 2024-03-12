5f4e03465bdc28c098307880b943125a3d84b965
MZubair <muhammadzubair78692110@gmail.com>
2024-03-13 01:18:24 +0500

    Offer_spiner_initial_commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// console.log("This is to check the linkage between HTML and JavaScript Files.");
+const spinbtn = document.querySelector('.spin-btn');
+const formSubmit = document.querySelector('#coupon-form');
+const wheel = document.querySelector('.wheel');
+let value = Math.ceil(Math.random() * 3600);
+console.log(formSubmit);
+formSubmit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let minRotations = 1;
+    wheel.style.transform = "rotate(" + value + "deg)";
+    value += minRotations + Math.ceil(Math.random() * 3600);
+    setInterval(() => {
+        formSubmit.children[0].value = '';
+        formSubmit.children[1].value = '';
+        window.location.href = "/assets/markUp/data_form.html"
+    }, 4000);
+})
