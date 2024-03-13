fdef5d35e552e251a0999ffe8344776e9b03be85
MZubair <muhammadzubair78692110@gmail.com>
2024-03-13 18:09:51 +0500

    rotateBtn_spin_added_commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,12 @@ const wheel = document.querySelector('.wheel');
 
 let value = Math.ceil(Math.random() * 3600);
 // console.log(formSubmit);
-
+const rotate = () => {
+    let minRotations = 1;
+    wheel.style.transform = "rotate(" + value + "deg)";
+    value += minRotations + Math.ceil(Math.random() * 3600);
+}
+spinbtn.addEventListener('click', rotate);
 
 formSubmit.addEventListener('submit', (e) => {
     e.preventDefault();
